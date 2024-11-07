f12f8d61863864c7bc17ffb5834a9683fc34f11f
Ludwig-E Dufour <ludwig-emmanuel@hotmail.com>
2024-11-07 08:43:01 -0500

    Revert "Add bluetooth 1.0"
    This reverts commit bf0f9a1ebb9d17196d416348af55835777a26dc1.

--- robus_mobile_app/lib/main.dart
+++ robus_mobile_app/lib/main.dart
@@ -1,55 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart' as bluePlus;
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart' as serial;
-import 'package:robus_mobile_app/views/BluetoothConnectionsPage.dart';
 import 'package:robus_mobile_app/views/drawing_page.dart';
 import 'package:robus_mobile_app/views/remote_page.dart';
 
 void main() {
   runApp(const AutomatismApp());
-}
-
-class BluetoothSetupPage extends StatefulWidget {
-  const BluetoothSetupPage({Key? key}) : super(key: key);
-
-  @override
-  _BluetoothSetupPageState createState() => _BluetoothSetupPageState();
-}
-
-class _BluetoothSetupPageState extends State<BluetoothSetupPage> {
-  late serial.BluetoothConnection _connection;
-
-  @override
-  void initState() {
-    super.initState();
-    _connectToDevice();
-  }
-
-  Future<void> _connectToDevice() async {
-    List<serial.BluetoothDevice> devices = await serial.FlutterBluetoothSerial.instance.getBondedDevices();
-
-    String deviceAddress = 'XX:XX:XX:XX:XX:XX';
-    serial.BluetoothDevice device = devices.firstWhere((d) => d.address == deviceAddress);
-
-    try {
-      _connection = await serial.BluetoothConnection.toAddress(device.address);
-      print('Connected to ${device.name}');
-    } catch (error) {
-      print('Error connecting to device: $error');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bluetooth Setup'),
-      ),
-      body: Center(
-        child: Text('Connecting to Bluetooth device...'),
-      ),
-    );
-  }
 }
 
 class AutomatismApp extends StatelessWidget {
@@ -74,7 +28,7 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('S1 Project'),
+        title: const Text('Automatism Project'),
       ),
       body: Center(
         child: Column(
@@ -102,18 +56,6 @@ class MainMenu extends StatelessWidget {
                 );
               },
               child: const Text('Robot Control Section'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const BluetoothConnectionsPage(),
-                  ),
-                );
-              },
-              child: const Text('Bluetooth Connections'),
             ),
           ],
         ),
